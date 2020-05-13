cbd3507daede904009be0fb4e207b1be1942bdd5
Elise Dindault <elise.dindault@gmail.com>
2020-05-13 14:29:13 +0200

    yelp app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
 end
